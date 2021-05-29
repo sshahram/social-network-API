50241fb9f1c4ba3c70a4ac43428f28e89c7bcccc
sshahram <shirin.shahram23@gmail.com>
2021-05-29 11:42:42 -0700

    add controllers to get all users and a single user

---
+++ controllers/user-controller.js
@@ -0,0 +1,41 @@
+const {User} = require('../models');
+
+const userController = {
+    // get all users
+    getAllUser(req, res) {
+        User.find({})
+            .select('-__v')
+            .sort({_id: -1})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // get a user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id})
+            .populate({
+                pth: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                // if no user us found, send 404
+                if(!dbUserData) {
+                    res.status(404).json({message: 'No user found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+}
