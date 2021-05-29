f5fe5f269839b42a58f5a12ca7dc3a88f05a1b41
sshahram <shirin.shahram23@gmail.com>
2021-05-29 11:50:57 -0700

    add a controller to update a user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -45,4 +45,17 @@ const userController = {
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.status(400).json(err));
     },
+
+    // update a user by id
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {new: true, runValidators: true})
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({message: 'No user found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
 }
