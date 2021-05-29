af3b53a995ef3e070d67f85d1ee5ed677eedfda9
sshahram <shirin.shahram23@gmail.com>
2021-05-29 14:19:05 -0700

    add a controller to get single thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -12,4 +12,21 @@ const thougthController = {
                 res.status(400).json(err);
             });
     },
+
+    // get a thought by if
+    getThoughtById({params}, res) {
+        Thought.findOne({_id: params.id})
+            .select('-__v')
+            .then(dbThoughtData => {
+                if(!dbThoughtData) {
+                    res.status(404).json({message: 'No thought found with this id!'});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 }
