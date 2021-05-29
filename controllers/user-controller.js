5ae54bfa95ac50acc67915088b4322ea1e8b531c
sshahram <shirin.shahram23@gmail.com>
2021-05-29 12:48:17 -0700

    create controller to add a friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -17,7 +17,7 @@ const userController = {
     getUserById({ params }, res) {
         User.findOne({ _id: params.id})
             .populate({
-                pth: 'thoughts',
+                path: 'thoughts',
                 select: '-__v'
             })
             .populate({
@@ -70,6 +70,34 @@ const userController = {
                 res.json(dbUserData);
             })
             .catch(err => res.status(400).json(err));
+    },
+
+    // add friend
+    addFriend({params}, res) {
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            {$push: {friends: body}},
+            {new: true, runValidators: true}
+        )
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({message: 'No user found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // remove friend
+    removeFriend({params}, res) {
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            {$pull: {friends: {_id: params.friendId}}},
+            {new: true}
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
     }
 };
 
